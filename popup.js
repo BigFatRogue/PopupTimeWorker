5aba01973bc86170e0b964a75912fc62b85745e6
p.golubev <bobik0193@mail.ru>
2024-03-15 17:23:48 +0300

    init

---
+++ popup.js
@@ -0,0 +1,7 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Ваш код здесь
+//     document.getElementById('next-page').addEventListener('click', function() {
+//         // Переход на другую страницу
+//         window.location.href = 'http://127.0.0.1:5000/authentication';
+//     });
+// });
