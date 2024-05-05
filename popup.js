5867967e3608137c996e1d0e04fa4f02fcb983b1
BigFatRogue <75310303+BigFatRogue@users.noreply.github.com>
2024-05-05 18:28:49 +0300

    Load pythonAnyWhere

--- popup.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Ваш код здесь
-//     document.getElementById('next-page').addEventListener('click', function() {
-//         // Переход на другую страницу
-//         window.location.href = 'http://127.0.0.1:5000/authentication';
-//     });
-// });
-
